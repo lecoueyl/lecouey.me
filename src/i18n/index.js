3887c06f508bd8ec6bcfa824a74a1032984dbffd
leo <leonard.lecouey@gmail.com>
2020-05-22 09:44:42 +0900

    Update packages and fix caconical links

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -18,6 +18,7 @@ module.exports = {
   defaultLocale: 'en',
   seo: true,
   lazy: true,
+  baseUrl: process.env.APP_URL,
   detectBrowserLanguage: {
     cookieKey: 'redirected',
     useCookie: true,
