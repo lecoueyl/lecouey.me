18a76a5d26558760d33904d0aa4f05c80844a2dd
Léonard Lecouey <leonard.lecouey@gmail.com>
2025-02-21 13:30:05 +0900

    fix: improve animation

--- app/components/Works.vue
+++ app/components/Works.vue
@@ -1,34 +1,34 @@
 <template>
-  <section class="container relative grid sm:grid-cols-6 gap-64 bg-neutral-100 pt-64">
-    <TransitionReveal tag="h2" class="col-span-full pt-4 flex items-start">
+  <section class="container relative grid sm:grid-cols-6 gap-8 bg-neutral-100 pt-64">
+    <TransitionReveal tag="h2" class="col-span-full pt-4 flex items-start mb-64">
       <span class="text-8xl">Works</span>
       <span class="pt-4 text-base font-normal text-neutral-500">(5)</span>
     </TransitionReveal>
 
-    <div class="sticky top-1/2 sm:col-span-4 sm:col-start-2 col-span-full overflow-hidden">
-      <div ref="title" class="w-full text-center text-4xl" />
+    <div class="sticky top-1/2 sm:col-span-3 sm:col-start-3 col-span-full overflow-hidden">
+      <div ref="title" class="text-4xl" />
     </div>
 
     <article
       :ref="(el) => setElementRef(el)"
-      class="relative z-10 sm:col-span-4 sm:col-start-2 gap-6"
-      data-title="title 1"
+      class="relative z-10 sm:col-span-4 sm:col-start-2 gap-6 mt-96"
+      data-title="Dream of fantasy"
     >
       <NuxtImg src="img/thumb1.jpg" alt="project 1" class="w-full rounded-2xl shadow-2xl" />
     </article>
 
     <article
       :ref="(el) => setElementRef(el)"
-      class="relative z-10 sm:col-span-4 sm:col-start-2 gap-6"
-      data-title="title 2"
+      class="relative z-10 sm:col-span-4 sm:col-start-2 gap-6 mt-96"
+      data-title="Secondary long title"
     >
       <NuxtImg src="img/thumb2.jpg" alt="project 2" class="w-full rounded-2xl shadow-2xl" />
     </article>
 
     <article
       :ref="(el) => setElementRef(el)"
-      class="relative z-10 sm:col-span-4 sm:col-start-2 gap-6"
-      data-title="title 3"
+      class="relative z-10 sm:col-span-4 sm:col-start-2 gap-6 mt-96"
+      data-title="Along the pain 3"
     >
       <NuxtImg src="img/thumb3.jpg" alt="project 3" class="w-full rounded-2xl shadow-2xl" />
     </article>
@@ -69,7 +69,7 @@ watch(currentTitle, async (newVal) => {
 
   await gsap.to(title.value, {
     y: isScrollingDown ? '100%' : '-100%',
-    duration: 0.5,
+    duration: 0.3,
     ease: 'circ2.inOut',
   });
   title.value.innerText = newVal;
@@ -78,8 +78,8 @@ watch(currentTitle, async (newVal) => {
     { y: isScrollingDown ? '-100%' : '100%' },
     {
       y: '0%',
-      duration: 0.5,
-      ease: 'circ2.inOut',
+      duration: 0.3,
+      ease: 'circ2.out',
     },
   );
 });
